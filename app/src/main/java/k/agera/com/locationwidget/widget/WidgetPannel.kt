4b6bc6a4e5b0bd6fba2c9b7fc68ccbfd6f287e57
Likai <Likai@chinasofti.com>
2018-06-13 16:56:07 +0800

    init repo

---
+++ app/src/main/java/k/agera/com/locationwidget/widget/WidgetPannel.kt
@@ -0,0 +1,22 @@
+package k.agera.com.locationwidget.widget
+
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+
+/**
+ * Created by Agera on 2018/6/13.
+ */
+class WidgetPannel:AppWidgetProvider() {
+
+    override fun onUpdate(context: Context?, appWidgetManager: AppWidgetManager?, appWidgetIds: IntArray?) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds)
+
+
+    }
+
+
+
+
+
+}
